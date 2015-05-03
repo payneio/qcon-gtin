aeaa25dce2497b0d9de64fbc0ab28a982ed37e1e
Paul Payne <paul@payne.io>
2015-05-02 23:32:41 -0700

    Correct launch_service health check hint.

--- launch_service.sh
+++ launch_service.sh
@@ -6,5 +6,5 @@ fleetctl start gtin.proxy.private.frontend
 fleetctl start gtin.proxy.public@{1..3}
 fleetctl start gtin.proxy.public.frontend
 
-echo "Health check: http://blue-api.qcon.demo/stores/health/gtins/check"
+echo "Health check: http://api-blue.qcon.demo/stores/health/gtins/check"
 echo "Health check: http://api.qcon.demo/stores/health/gtins/check"
